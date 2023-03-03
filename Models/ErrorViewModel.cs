4b7d5699d92201f76e9dfa44aad24795e7af4adb
chamlamh <70345325+chamlamh@users.noreply.github.com>
2023-03-02 19:47:45 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreshBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
