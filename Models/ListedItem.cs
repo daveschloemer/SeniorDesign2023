16bfb8b6f899bd9a95d181c89ee0ea1e80276699
chamlamh <70345325+chamlamh@users.noreply.github.com>
2023-03-19 16:13:59 -0400

    Added Listed Book

---
+++ Models/ListedItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FreshBooks.Models
+{
+    public class ListedItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+
+        public int BookId { get; set; }
+        [ForeignKey("BookId")]
+        public Book Book { get; set; }
+
+
+        public int ListedId { get; set; }
+        [ForeignKey("ListedId")]
+        public Listed Listed { get; set; }
+    }
+}
