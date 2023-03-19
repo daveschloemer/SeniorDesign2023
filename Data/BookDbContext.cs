08577cf3c9cde2e61a33e71f221fdc8d7e2f7b9a
David Schloemer <schloede@mail.uc.edu>
2023-03-18 21:47:58 -0400

    Identity

--- Data/BookDbContext.cs
+++ Data/BookDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using FreshBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FreshBooks.Data
 {
-    public class BookDbContext : DbContext
+    public class BookDbContext : IdentityDbContext<ApplicationUser>
     {
         public BookDbContext(DbContextOptions<BookDbContext> options) : base(options) 
         {
@@ -12,14 +13,15 @@ namespace FreshBooks.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-/*            modelBuilder.Entity<Author_Books>().HasKey(ab => new
-            {
-                ab.AuthorId,
-                ab.BookId
-            });
-            modelBuilder.Entity<Author_Books>().HasOne(b => b.Book).WithMany(ab => ab.Author_Books).HasForeignKey(b => b.BookId);
-            modelBuilder.Entity<Author_Books>().HasOne(b => b.Author).WithMany(ab => ab.Author_Books).HasForeignKey(b => b.AuthorId);
-            base.OnModelCreating(modelBuilder);*/
+            base.OnModelCreating(modelBuilder);
+            /*            modelBuilder.Entity<Author_Books>().HasKey(ab => new
+                        {
+                            ab.AuthorId,
+                            ab.BookId
+                        });
+                        modelBuilder.Entity<Author_Books>().HasOne(b => b.Book).WithMany(ab => ab.Author_Books).HasForeignKey(b => b.BookId);
+                        modelBuilder.Entity<Author_Books>().HasOne(b => b.Author).WithMany(ab => ab.Author_Books).HasForeignKey(b => b.AuthorId);
+                        base.OnModelCreating(modelBuilder);*/
         }
 
 /*        public DbSet<Author> Author { get; set; }
