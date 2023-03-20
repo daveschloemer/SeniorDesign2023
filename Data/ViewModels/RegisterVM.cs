a6c21e3538e9003ea7bab1561ff6ac31fff02670
David Schloemer <schloede@mail.uc.edu>
2023-03-20 19:12:19 -0400

    update password requirements

--- Data/ViewModels/RegisterVM.cs
+++ Data/ViewModels/RegisterVM.cs
@@ -13,10 +13,12 @@ namespace FreshBooks.Data.ViewModels
         [Required(ErrorMessage = "Email Address is Required")]
         [RegularExpression(@"^[a-zA-Z0-9._%+-]+@mail\.uc\.edu$", ErrorMessage = "Please enter a valid email address on the mail.uc.edu domain.")]
         public string EmailAddress { get; set; }
-  
+
         [Required]
         [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[\W_]).+$", ErrorMessage = "Password must contain at least one uppercase letter and one special character.")]
         public string Password { get; set; }
+
 
         [Display(Name = "Confirm Password")]
         [Required(ErrorMessage = "Confirm Password is Required")]
